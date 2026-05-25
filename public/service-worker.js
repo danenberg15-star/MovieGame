121f6a7b5453e2780a9ed1038a2a14d0752c6f34
unknown <danenberg15@gmail.com>
2026-05-25 16:09:52 +0300

    INCREASE CINEMASTER FONTV2

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker for CINEMASTER PWA
-const CACHE_NAME = 'cinemaster-v1.4.0';
+const CACHE_NAME = 'cinemaster-v1.5.0';
 
 // Only cache essential files that we know exist
 const ESSENTIAL_CACHE = [
